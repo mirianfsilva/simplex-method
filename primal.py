09278d0fcdf7e1db78690bb327648cc4cd0b12aa
Mírian <contato.mirianfs@gmail.com>
2018-06-05 01:12:56 -0300

    output updated to file with decimal precision

--- primal.py
+++ primal.py
@@ -23,23 +23,35 @@ def map(f, it):
 ''' Função para checar o tableau pra saber se a PL é ilimitada, caso seja, retorna um certificado
 '''	
 
-def solution_unbounded(tableau, ratio, columns, index_column,output_2):
+def solution_unbounded(tableau, ratio, columns, index_column):
 	if (ratio == math.inf):
-		cert = [0 for _ in range(0, len(tableau[0]))] #vetor para o certificado
+		cert = [0 for _ in range(0, len(tableau[0])-1)] #vetor para o certificado
 		for i in range(1, len(tableau)):
-				for j in range(0, columns):
-					if (tableau[i][j] == 1):
+				for j in range(0, len(tableau[0])-1):
+					#print ('tableau[',i,'][',j,']',tableau[i][j])
+					if tableau[i][j] == 1:
+						#print("linha: ",i,"coluna: ",j)
 						for k in range(0, len(tableau)):
-							if (tableau[k][j] == 0):
-								tableau[i][index_column] = 1
-								cert[j] = round((tableau[i][index_column]),3)
-							break
-		output_2.write(unbounded,'\n',cert)
+							if tableau[k][j] != 0:
+								#print("POSIÇÃO != 0: ",k,j)
+								#print ("TABLEAU [i][index_column]: ", tableau[i][index_column])
+								if tableau[i][index_column] == 0: 
+									cert[j] = round((tableau[i][index_column]),3)
+								elif tableau[i][index_column] > 0 or tableau[i][index_column] < 0: 
+									cert[j] = (-1)*round((tableau[i][index_column]),3)
+								#print("INDEX ->> ",cert[j])
+							cert[index_column] = 1 
+					elif tableau[i][index_column] > 0 or tableau[i][index_column] < 0:
+						continue
+					break
+		output_2 = [unbounded,cert]
+		with open("conclusão.txt","w") as file_2:
+			file_2.write('\n'.join(map(str, output_2)))
 		sys.exit()
 		
 ''' Função de pivotamento primal, caso b > 0 e tenha valores < 0 em c, que nesse caso é a primeira linha do tableau[0]
 '''    
-def primal_pivot(tableau, _aux, lines, columns, index_column,output_2):
+def primal_pivot(tableau, _aux, lines, columns, index_column):
 	ratio = math.inf
 	index_line = math.inf
 	#escolhe a linha baseado na menor razao positiva
@@ -50,8 +62,10 @@ def primal_pivot(tableau, _aux, lines, columns, index_column,output_2):
 				ratio = curr
 				index_line = i
 
-	solution_unbounded(tableau, ratio, columns, index_column,output_2)
-
+	#output_2 = 
+	solution_unbounded(tableau, ratio, columns, index_column)
+	#if solution_unbounded(tableau, ratio, columns, index_column): 
+	
 	denominator = tableau[index_line][index_column]
 	#divide a linha pelo valor temporario de um divisor calculado acima
 	for i in range(0, len(tableau[0])):
@@ -61,6 +75,3 @@ def primal_pivot(tableau, _aux, lines, columns, index_column,output_2):
 	
 	change_tableau(tableau, _aux,index_line,index_column)
 	
-
-
-
