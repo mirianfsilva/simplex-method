8da3364bb7fcba63ca9f8959c653087439ed6862
Mírian <contato.mirianfs@gmail.com>
2018-06-10 16:23:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 #Python 3.6.1 (Dec 2015) [GCC 4.8.2] on linux
+'''	PESQUISA OPERACIONAL 
+	Mírian Francielle da Silva 
+'''
+
 import ast, sys
 import tableauform
 from dual import dual_pivot
 from primal import primal_pivot
-from fractions import Fraction
 import numpy as np
-import json
 
-#from decimal import getcontext, Decimal
-# Set the precision.
-#getcontext().prec = 2
-
-#precisão das casas decimais na saída do problema
 class prettyfloat(float):
     def __repr__(self):
         return "%0.2f" % self
 
-#pra usar em listas de valores que podem ser iteradas
-#alterar as casas decimais de uma lista
 def map(f, it):
     result = []
     for x in it:
         result.append(f(x))
     return result
 
-#printar tableau
 def print_tableau(tableau):
 	
 	for i in range(0, len(tableau)):
@@ -32,7 +26,6 @@ def print_tableau(tableau):
 		#print(tableau[i])
 	#print('\n')
 			
-	#a cada chamada o arquivo é atualizado, então só a ultima fase do pivotamento vai ser impressa no arquivo, porque vai ser a ultima sobreposição
 	with open("pivotamento.txt", "w") as file_1:
 		for i in range(0, len(tableau)):
 			file_1.write('[')
@@ -40,17 +33,22 @@ def print_tableau(tableau):
 			file_1.write(']')
 			file_1.write('\n')
 	
-
-''' A saída deve conter a seguinte informação: Dizer se o problema é inviável (INFEASIBLE) com output = 0; ou dizer que o problema é viável mas possui solução ilimitada (FEASIBLE & UNBOUNDED) com output = 1; ou se o problema é viável e limitado, sendo assim possui solução ótima, (FEASIBLE & BOUNDED) com output = 2, e a solução ótima contendo os valores das variáveis {x1,..., xn}; isto é, valores das variáveis e valor objetivo da solução ótima. 
+''' A saída deve conter a seguinte informação: 
+Dizer se o problema é inviável (INFEASIBLE) com output = 0; 
+ou dizer que o problema é viável mas possui solução ilimitada (FEASIBLE & UNBOUNDED) com output = 1; 
+ou se o problema é viável e limitado, sendo assim possui solução ótima, (FEASIBLE & BOUNDED) com output = 2, 
+e a solução ótima contendo os valores das variáveis {x1,..., xn}; isto é, valores das variáveis e valor 
+objetivo da solução ótima. 
 '''
 infeasible = 0
 unbounded = 1
 optimal = 2
 
-''' Sempre que a entrada do programa é inviável ou a solução é ilimitada, a saída do modo detalhado deve dar alguma evidência para essa conclusão, sendo assim apresentar um certificado.
+''' Sempre que a entrada do programa é inviável ou a solução é ilimitada,
+a saída do modo detalhado deve dar alguma evidência para essa conclusão, 
+sendo assim apresentar um certificado.
 '''
 
-#função que retorna o vetor solução
 def solution(tableau, lines, columns):
 	var_solution = [0 for _ in range(0,columns)]
 	#print (len(tableau),columns)
@@ -65,9 +63,7 @@ def solution(tableau, lines, columns):
 						#print (var_solution)
 						break
 	return (var_solution)
-	
-''' Função que cria uma matriz para armazenar as operacoes realizadas durante o pivotamento no tableau
-'''
+
 def matrix_ope(matrix, lines, columns):
 	#matriz de operacoes com posições vazias
 	_aux = [[] for i in range(len(matrix))]
@@ -83,7 +79,10 @@ def matrix_ope(matrix, lines, columns):
 				_aux[i].append(0)
 	return _aux
 
-''' Procedimento que realiza o simplex na matriz já em FPI. A função verifica se é possível realizar o dual primeiro na matriz, caso contrário, faz o procedimento de pivotamento primal, e recebe a matriz, linhas e colunas como parametro como especificado da descrição do trabalho.
+''' Procedimento que realiza o simplex na matriz já em FPI. 
+A função verifica se é possível realizar o dual primeiro na matriz, 
+caso contrário, faz o procedimento de pivotamento primal, e recebe a matriz, 
+linhas e colunas como parametro como especificado da descrição do trabalho.
 '''
 def simplex(matrix, lines, columns):
 	
@@ -129,17 +128,10 @@ def simplex(matrix, lines, columns):
 	#print('\n'.join(map(str, output_2)))
 	#print('', optimal,'\n',var_solution,'\n', v_obj,'\n', cert)
 
-
-#saida no arquivo	
 def print_output1(arg):
 	with open("pivotamento.txt","w") as output_1:
 		output_1.write(arg)
 	
-''' Ler arquivo usando: ast – Abstract Syntax Trees
-    Tentar essa leitura caso literal_eval não funcione: 
-    str(self.lostfeatures).replace('[array([[','').replace(']], dtype=float32)
-    funçaõ l.strip remove caracters da linha
-'''
 with open("input.txt", "r") as csvfile:
 	count = 0
 	for l in csvfile:
@@ -156,7 +148,3 @@ with open("input.txt", "r") as csvfile:
 #print (lines, columns, matrix)
 
 simplex(matrix, lines, columns)
-
-
-#https://www.kdnuggets.com/2017/03/working-numpy-matrices.html
-#https://docs.python.org/3.1/library/fractions.html
